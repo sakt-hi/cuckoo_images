3f1ff282342ee88106e5c21f64d601db5752ec8e
sakt-hi <sakthivel.ganesan@hotmail.com>
2023-12-24 19:32:29 +0530

    chunk

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/cuckoo_images/'
+  base:'/cuckoo_images/',
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
